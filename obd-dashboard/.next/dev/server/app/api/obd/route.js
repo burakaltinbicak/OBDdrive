1c03b005945f1a142fe6e60c3c271df08ee055a1
burakaltinbicak <mslmbrkaltnbcak@gmail.com>
2026-03-02 12:02:30 +0300

    fix: remove .next from tracking

--- obd-dashboard/.next/dev/server/app/api/obd/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/obd/route.js")
-R.c("server/chunks/[root-of-the-server]__fbaf456a._.js")
-R.c("server/chunks/[root-of-the-server]__76a20098._.js")
-R.c("server/chunks/_next-internal_server_app_api_obd_route_actions_6e4af815.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/obd/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/obd/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- obd-dashboard/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"07S2j7HVkLw4jmNl4yq3KR+azUz7vILrwGaimIPpL+Q=\"\n}"
